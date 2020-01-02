91afc6b69f1220e3cc68a303550e865cb7d3db2d
Eric Manning <emanningmusic@yahoo.com>
2020-01-02 18:24:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
   end 
 
 def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    sql = "UPDATE student SET name = ?, grade = ? WHERE name = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.name)
   end
 
